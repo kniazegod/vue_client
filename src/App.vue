77949b1552f1f17e28dcc48c4b465f4cc39e5120
Roman Kniazev <kniazegod@gmail.com>
2019-05-28 22:01:47 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button class="button" @click="sendData">{{ buttonText }}</button>
+      <div class="info-wrapper">
+        <div v-if="!connected">Подключение к серверу...</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { setTimeout } from 'timers'
+
+const wsUrl = "ws://pm.tada.team/ws"
+const states = ['Медвед', 'Превед', '']
+let state = 2
+let ws = null
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      connected: false,
+      buttonText: '',
+    }
+  },
+  created: function () {
+    ws = new WebSocket(wsUrl)
+    this.addHandlers()
+  },
+  methods: {
+    addHandlers() {
+      ws.onopen = () => {
+        this.connected = true
+      };
+      ws.onclose = () => {
+        this.connected = false
+        console.log('Попытка реконекта')
+        setTimeout(() => {
+          ws = new WebSocket(wsUrl)
+          this.addHandlers()
+        }, 1000)
+      };
+      ws.onmessage = (event) => {
+        state = JSON.parse(event.data).state
+        this.buttonText = states[state]
+      };
+      ws.onerror = (error) => {
+        console.log("Ошибка " + error.message);
+      };
+    },
+    sendData() {
+      if (this.connected) {
+        ws.send(JSON.stringify({ state: state ? 0 : 1 }))
+      }
+    }
   }
 }
 </script>
 
 <style>
+html, body {
+    height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.button {
+  width: 200px;
+  height: 60px;
+  font-size: 30px;
+  background: #00a5ff;
+  border: none;
+  color: white;
+}
+.button:active {
+  background: #008fdd;
+}
+.info-wrapper {
+  margin-top: 10px;
+  height: 50px;
 }
 </style>
